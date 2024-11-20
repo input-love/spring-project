33ec2ef717b2ce41eac8362a68f3782d6becd97c
Andrew Samardak <samardak_andrey149@mail.ru>
2024-11-20 13:00:29 +0300

    feat | Add CRUD controller for clients table

---
+++ src/main/java/andrew/samardak/spring_aop/controller/ClientController.java
@@ -0,0 +1,56 @@
+package andrew.samardak.spring_aop.controller;
+
+import andrew.samardak.spring_aop.aspect.LogDataSourceError;
+import andrew.samardak.spring_aop.dto.request.ClientRequestDto;
+import andrew.samardak.spring_aop.dto.response.ClientResponseDto;
+import andrew.samardak.spring_aop.service.ClientService;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@LogDataSourceError
+@RequiredArgsConstructor
+@RequestMapping("v1/clients")
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class ClientController {
+
+    ClientService service;
+
+    @PostMapping
+    public ClientResponseDto create(
+            @RequestBody ClientRequestDto dto
+    ) {
+        return service.create(dto);
+    }
+
+    @GetMapping("/{id}")
+    public ClientResponseDto read(
+            @PathVariable Long id
+    ) {
+        return service.read(id);
+    }
+
+    @PatchMapping("/{id}")
+    public ClientResponseDto update(
+            @PathVariable Long id,
+            @RequestBody ClientRequestDto dto
+    ) {
+        return service.update(dto, id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(
+            @PathVariable Long id
+    ) {
+        service.delete(id);
+    }
+}
