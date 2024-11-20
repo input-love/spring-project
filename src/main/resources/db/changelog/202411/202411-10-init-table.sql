ddf6c2c59f80d842d1754effbe57cb1f69d2fb6b
Andrew Samardak <samardak_andrey149@mail.ru>
2024-11-20 20:14:37 +0300

    migration | Add relationships between transactions, accounts, and clients tables

--- src/main/resources/db/changelog/202411/202411-10-init-table.sql
+++ src/main/resources/db/changelog/202411/202411-10-init-table.sql
@@ -1,35 +1,3 @@
--- Создание таблицы accounts
-CREATE TABLE accounts
-(
-    id             BIGSERIAL PRIMARY KEY,
-    account_type   VARCHAR(255)   NOT NULL,
-    account_status VARCHAR(255)   NOT NULL,
-    balance        NUMERIC(10, 2) NOT NULL,
-    frozen_amount  NUMERIC(10, 2) NOT NULL
-);
-
-COMMENT ON TABLE accounts IS 'Таблица для хранения информации об аккаунтах пользователей';
-COMMENT ON COLUMN accounts.id IS 'Уникальный идентификатор аккаунта';
-COMMENT ON COLUMN accounts.account_type IS 'Тип аккаунта (например: дебетовый счет или кредитный)';
-COMMENT ON COLUMN accounts.account_status IS 'Статус аккаунта (например: открытый или арестованный)';
-COMMENT ON COLUMN accounts.balance IS 'Баланс на аккаунте';
-COMMENT ON COLUMN accounts.frozen_amount IS 'Сумма заблокированных транзакций';
-
--- Создание таблицы transactions
-CREATE TABLE transactions
-(
-    id                 BIGSERIAL PRIMARY KEY,
-    transaction_status VARCHAR(255)   NOT NULL,
-    amount             NUMERIC(10, 2) NOT NULL,
-    transaction_time   TIMESTAMP      NOT NULL
-);
-
-COMMENT ON TABLE transactions IS 'Таблица для хранения транзакций по аккаунтам';
-COMMENT ON COLUMN transactions.id IS 'Уникальный идентификатор транзакции';
-COMMENT ON COLUMN transactions.transaction_status IS 'Статус транзакции (например: принята или запрошена)';
-COMMENT ON COLUMN transactions.amount IS 'Сумма транзакции';
-COMMENT ON COLUMN transactions.transaction_time IS 'Время выполнения транзакции';
-
 -- Создание таблицы clients
 CREATE TABLE clients
 (
@@ -48,6 +16,42 @@ COMMENT ON COLUMN clients.last_name IS 'Фамилия клиента';
 COMMENT ON COLUMN clients.middle_name IS 'Отчество клиента';
 COMMENT ON COLUMN clients.blocked_for IS 'Флаг, указывающий, заблокирован ли клиент';
 COMMENT ON COLUMN clients.blocked_whom IS 'Причина блокировки клиента или информация о заблокированных';
+
+-- Создание таблицы accounts
+CREATE TABLE accounts
+(
+    id             BIGSERIAL PRIMARY KEY,
+    account_type   VARCHAR(255)   NOT NULL,
+    account_status VARCHAR(255)   NOT NULL,
+    balance        NUMERIC(10, 2) NOT NULL,
+    frozen_amount  NUMERIC(10, 2) NOT NULL,
+    client_id      BIGINT REFERENCES clients (id)
+);
+
+COMMENT ON TABLE accounts IS 'Таблица для хранения информации об аккаунтах пользователей';
+COMMENT ON COLUMN accounts.id IS 'Уникальный идентификатор аккаунта';
+COMMENT ON COLUMN accounts.account_type IS 'Тип аккаунта (например: дебетовый счет или кредитный)';
+COMMENT ON COLUMN accounts.account_status IS 'Статус аккаунта (например: открытый или арестованный)';
+COMMENT ON COLUMN accounts.balance IS 'Баланс на аккаунте';
+COMMENT ON COLUMN accounts.frozen_amount IS 'Сумма заблокированных транзакций';
+COMMENT ON COLUMN accounts.client_id IS 'Таблица для хранения информации о клиентах';
+
+-- Создание таблицы transactions
+CREATE TABLE transactions
+(
+    id                 BIGSERIAL PRIMARY KEY,
+    transaction_status VARCHAR(255)   NOT NULL,
+    amount             NUMERIC(10, 2) NOT NULL,
+    transaction_time   TIMESTAMP      NOT NULL,
+    account_id         BIGINT REFERENCES accounts (id)
+);
+
+COMMENT ON TABLE transactions IS 'Таблица для хранения транзакций по аккаунтам';
+COMMENT ON COLUMN transactions.id IS 'Уникальный идентификатор транзакции';
+COMMENT ON COLUMN transactions.transaction_status IS 'Статус транзакции (например: принята или запрошена)';
+COMMENT ON COLUMN transactions.amount IS 'Сумма транзакции';
+COMMENT ON COLUMN transactions.transaction_time IS 'Время выполнения транзакции';
+COMMENT ON COLUMN transactions.account_id IS 'Таблица для хранения информации об аккаунтах пользователей';
 
 -- Создание таблицы error_logs
 CREATE TABLE error_logs
--- src/main/resources/db/changelog/202411/202411-20-fill-data.sql
+++ src/main/resources/db/changelog/202411/202411-20-fill-data.sql
@@ -1,16 +1,17 @@
--- Вставка данных в таблицу accounts
-INSERT INTO accounts (account_type, account_status, balance, frozen_amount)
-VALUES ('DEBIT', 'OPEN', 1000.00, 0.00),
-       ('CREDIT', 'OPEN', 1500.00, 0.00);
-
--- Вставка данных в таблицу transactions
-INSERT INTO transactions (transaction_status, amount, transaction_time)
-VALUES ('ACCEPTED', 250.00, '2024-10-01 10:30:00'),
-       ('ACCEPTED', 75.50, '2024-10-02 11:00:00'),
-       ('ACCEPTED', 1000.00, '2024-10-03 11:30:00');
-
--- Вставка данных в таблицу client
+-- Вставка данных в таблицу clients
 INSERT INTO clients (first_name, last_name, middle_name, blocked_for, blocked_whom)
 VALUES ('John', 'Doe', 'Michael', false, NULL),
        ('Jane', 'Smith', 'Alice', true, 'Fraud detected'),
        ('Emily', 'Davis', 'Sarah', true, 'Policy violation');
+
+-- Вставка данных в таблицу accounts
+INSERT INTO accounts (account_type, account_status, balance, frozen_amount, client_id)
+VALUES ('DEBIT', 'OPEN', 1000.10, 0.00, 3),
+       ('CREDIT', 'OPEN', 1500.15, 0.00, 2),
+       ('CREDIT', 'OPEN', 2000.20, 0.00, 1);
+
+-- Вставка данных в таблицу transactions
+INSERT INTO transactions (transaction_status, amount, transaction_time, account_id)
+VALUES ('ACCEPTED', 1000.10, '2024-10-01 10:30:00', 3),
+       ('ACCEPTED', 1500.15, '2024-10-02 11:00:00', 2),
+       ('ACCEPTED', 2000.20, '2024-10-03 11:30:00', 1);
