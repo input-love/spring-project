ed3004db676615d05a292489b6848408f1b6b561
Andrew Samardak <samardak_andrey149@mail.ru>
2024-11-21 22:40:09 +0300

    refactor | Update toDto methods in mappers to include IDs from nested entities

--- src/main/java/andrew/samardak/spring_aop/mappers/AccountMapper.java
+++ src/main/java/andrew/samardak/spring_aop/mappers/AccountMapper.java
@@ -4,7 +4,12 @@ import andrew.samardak.spring_aop.dto.request.AccountRequestDto;
 import andrew.samardak.spring_aop.dto.response.AccountResponseDto;
 import andrew.samardak.spring_aop.entity.Account;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface AccountMapper extends BaseMapper<Account, AccountRequestDto, AccountResponseDto> {
+
+    @Override
+    @Mapping(source = "client.id", target = "clientId")
+    AccountResponseDto toDto(Account account);
 }
--- src/main/java/andrew/samardak/spring_aop/mappers/TransactionMapper.java
+++ src/main/java/andrew/samardak/spring_aop/mappers/TransactionMapper.java
@@ -4,7 +4,12 @@ import andrew.samardak.spring_aop.dto.request.TransactionRequestDto;
 import andrew.samardak.spring_aop.dto.response.TransactionResponseDto;
 import andrew.samardak.spring_aop.entity.Transaction;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface TransactionMapper extends BaseMapper<Transaction, TransactionRequestDto, TransactionResponseDto> {
+
+    @Override
+    @Mapping(source = "account.id", target = "accountId")
+    TransactionResponseDto toDto(Transaction transaction);
 }
