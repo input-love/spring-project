92e640d5dde023c3c60ae9361f0cc40d5fd4fc11
Andrew Samardak <samardak_andrey149@mail.ru>
2024-11-23 01:16:50 +0300

    feat | Add KafkaTransactionAcceptProducer for sending data to topic

---
+++ src/main/java/andrew/samardak/spring_aop/kafka/producer/KafkaTransactionAcceptProducer.java
@@ -0,0 +1,46 @@
+package andrew.samardak.spring_aop.kafka.producer;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.header.Headers;
+import org.apache.kafka.common.header.internals.RecordHeader;
+import org.apache.kafka.common.header.internals.RecordHeaders;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Service;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+
+@Service
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class KafkaTransactionAcceptProducer<V> {
+
+    KafkaTemplate<String, V> kafkaTemplate;
+
+    public CompletableFuture<SendResult<String, V>> sendMessage(
+            V message,
+            Map<String, String> headers
+    ) {
+        Headers recordHeaders = new RecordHeaders();
+        headers.forEach((key, value) ->
+                recordHeaders.add(new RecordHeader(key, value.getBytes(StandardCharsets.UTF_8)))
+        );
+
+        ProducerRecord<String, V> record = new ProducerRecord<>(
+                "t1_demo_transaction_accept",
+                null,
+                null,
+                UUID.randomUUID().toString(),
+                message,
+                recordHeaders
+        );
+
+        return kafkaTemplate.send(record);
+    }
+}
