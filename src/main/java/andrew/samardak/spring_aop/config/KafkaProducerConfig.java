f2193d9ca7432921dd48bf884dc4a013cca57882
Andrew Samardak <samardak_andrey149@mail.ru>
2024-11-23 01:13:07 +0300

    refactor | Remove default topic initialization logic from KafkaTemplate bean

--- src/main/java/andrew/samardak/spring_aop/config/KafkaProducerConfig.java
+++ src/main/java/andrew/samardak/spring_aop/config/KafkaProducerConfig.java
@@ -1,6 +1,5 @@
 package andrew.samardak.spring_aop.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -9,15 +8,10 @@ import org.springframework.kafka.core.ProducerFactory;
 @Configuration
 public class KafkaProducerConfig {
 
-    @Value("${spring-project.kafka.topic.metrics}")
-    private String metricsTopic;
-
     @Bean
     public KafkaTemplate<String, Object> kafkaTemplate(
             ProducerFactory<String, Object> producerFactory
     ) {
-        KafkaTemplate<String, Object> template = new KafkaTemplate<>(producerFactory);
-        template.setDefaultTopic(metricsTopic);
-        return template;
+        return new KafkaTemplate<>(producerFactory);
     }
 }
