5c8593240bd9764b48dd3451a7656c4a9ce33e6e
Andrew Samardak <samardak_andrey149@mail.ru>
2024-11-28 19:33:00 +0300

    feat | Add JPQL query for calculating transaction amount sum

--- src/main/java/andrew/samardak/spring_aop/repository/TransactionRepository.java
+++ src/main/java/andrew/samardak/spring_aop/repository/TransactionRepository.java
@@ -2,6 +2,7 @@ package andrew.samardak.spring_aop.repository;
 
 import andrew.samardak.spring_aop.entity.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
@@ -10,7 +11,8 @@ import java.util.List;
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 
-    BigDecimal sumAmountByAccountId(Long accountId);
-
     List<Transaction> findByAccountId(Long accountId);
+
+    @Query("SELECT SUM(t.amount) FROM Transaction t WHERE t.account.id = :accountId")
+    BigDecimal sumAmountByAccountId(Long accountId);
 }
