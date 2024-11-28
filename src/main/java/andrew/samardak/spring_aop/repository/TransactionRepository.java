d8528ffcdf6e0ed523fc2ee3fd3109e804dd37ba
Andrew Samardak <samardak_andrey149@mail.ru>
2024-11-28 19:23:12 +0300

    feat | Add sumAmountByAccountId and findByAccountId methods to TransactionRepository

--- src/main/java/andrew/samardak/spring_aop/repository/TransactionRepository.java
+++ src/main/java/andrew/samardak/spring_aop/repository/TransactionRepository.java
@@ -4,6 +4,13 @@ import andrew.samardak.spring_aop.entity.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+    BigDecimal sumAmountByAccountId(Long accountId);
+
+    List<Transaction> findByAccountId(Long accountId);
 }
